1cb53b4883c3c4a1dcae9b40441955b2eddf9547
Cameron E Baird <37827103+Camelron@users.noreply.github.com>
2018-11-03 13:01:50 -0700

    jump script wip

--- Fall2k18Jam/Assets/hopScript.cs
+++ Fall2k18Jam/Assets/hopScript.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class hopScript : MonoBehaviour {
-    Vector2 mouseInit;
+    Vector3 mouseInit;
     bool dragging;
     Camera cameron;
+    public GameObject food;
+
+    public int MAX_POWER;
+    public int JUMP_FRAMES;
     
 
 	// Use this for initialization
 	void Start () {
-        mouseInit = Vector2.zero;
+        mouseInit = Vector3.zero;
         dragging = false;
         cameron = Camera.main;
         
@@ -20,30 +24,65 @@ public class hopScript : MonoBehaviour {
     void Update() {
         if (Input.GetKeyDown(KeyCode.Mouse0)) { 
             mouseInit = Input.mousePosition;
-            
-            
+            StartCoroutine("Charge");
         }
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            jump(Input.mousePosition);
-        }
+        
+        
         cameron.transform.LookAt(this.gameObject.transform.position);
         Debug.DrawRay(transform.position, transform.forward * 8);
 	}
 
-    //dont touch
-    private void jump(Vector2 mouseFin)
+  
+
+    IEnumerator Charge()
     {
-        Vector3 line = Vector3.Normalize(mouseFin - mouseInit);
+        Debug.Log("started charge");
+        int timer = 0;
+        while (true)
+        {
+            Vector3 line = Vector3.Normalize(Input.mousePosition - mouseInit);
+            line.z = line.y;
+            line.y = 0;
+            transform.LookAt(transform.position - line);
+            if (Input.GetKeyUp(KeyCode.Mouse0)) { 
+                Debug.Log("stop stop STOP!");
+                break;
+            }
+            if(timer < MAX_POWER)
+            {
+                timer++;
+            }
+            yield return 0;
+        }
+        IEnumerator jumpCoroutine = Jump(timer * 1f * Time.deltaTime);
+        StartCoroutine(jumpCoroutine);
         
+    }
+
+    //dont touch
+    IEnumerator Jump(float distance)
+    {
+        float initYPos = transform.position.y;
+        float xpos = 0;
+        while (xpos < distance)
+        {
+            xpos += distance / JUMP_FRAMES;
+            transform.position += transform.forward * distance / JUMP_FRAMES;
+            float quadSol = (-1 * Mathf.Pow(xpos, 2)) + (distance * xpos);
+            transform.position = new Vector3(transform.position.x, initYPos + quadSol, transform.position.z);
+            
+            yield return 0;
+        }
+        transform.position = new Vector3(transform.position.x, initYPos, transform.position.z);
+
+        /*
         Debug.Log("vector: " + line);
         Vector3 goTo = transform.position - transform.right * line.x;
         goTo -= transform.forward * line.y;
-        float rot = Vector3.Angle(new Vector3(0, -1, 0), line);
-        transform.RotateAround(Vector3.up, Mathf.Deg2Rad * rot);
+        
         transform.position = goTo;
 
-        
+        */
         
     }
 }
