773041b516a68e8931feb9935225819184acd8e3
Sukeerth Vegaraju <ssvegaraju@gmail.com>
2018-11-02 22:22:54 -0700

    Behaviour tree done.

---
+++ Fall2k18Jam/Assets/Scripts/TestBT.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBT : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		List<TreeNode> nodes = new List<TreeNode>();
+		nodes.Add(new TreeNode(delegate() {return BTEvaluationResult.Success;}, 
+			delegate() {Debug.Log("Success");}, 
+			delegate() {Debug.Log("Failure");}));
+		BT bt = new BT(nodes);
+		StartCoroutine(bt.Tick());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
