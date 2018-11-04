2d7c6f84f7d339ed98c88c3a00dd73622390c87d
Jose Pacio <pacioj@uw.edu>
2018-11-04 00:52:14 -0700

    cactus anims and bad guys

---
+++ Fall2k18Jam/Assets/Scripts/RagdollSwitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollSwitch : MonoBehaviour {
+
+    Rigidbody[] rigidbodies;
+    Animator anim;
+    public float ragdollTime = 1;
+    public float deathAnimTime = 1;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody r in rigidbodies) {
+            r.isKinematic = true;
+        }
+        death();
+	}
+
+    void death() {
+        Invoke("turnOnRagdoll", deathAnimTime);
+    }
+
+
+    void turnOnRagdoll() {
+        if (anim != null) {
+            anim.enabled = false;
+        }
+
+        foreach (Rigidbody r in rigidbodies) {
+            r.isKinematic = false;
+            r.AddForce(-transform.forward * 500 + transform.up * 200);
+        }
+
+        Invoke("turnOffRagdoll", ragdollTime);
+    }
+
+    void turnOffRagdoll() {
+        foreach (Rigidbody r in rigidbodies)
+        {
+            r.isKinematic = true;
+        }
+    }
+}
