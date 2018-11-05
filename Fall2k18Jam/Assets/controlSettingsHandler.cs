c55e7c03c3b236024d5ecb6c390e8d79e604d461
Cameron E Baird <37827103+Camelron@users.noreply.github.com>
2018-11-04 16:38:11 -0800

    end of sunday cam

---
+++ Fall2k18Jam/Assets/controlSettingsHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controlSettingsHandler : MonoBehaviour {
+
+    public static bool mouseControl;
+    public GameObject targetButton;
+
+    private Text target;
+
+	// Use this for initialization
+	void Start () {
+        if(targetButton != null)
+            target = targetButton.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+	}
+
+    public bool getMouseControls()
+    {
+        return mouseControl;
+    }
+
+    public void toggle()
+    {
+        mouseControl = !mouseControl;
+        string output = "KEYBOARD";
+        if (mouseControl)
+            output = "MOUSE";
+
+        target.text = "Control Mode: " + output;
+    }
+}
---
+++ Fall2k18Jam/Assets/menuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void startGame()
+    {
+        Debug.Log("loading main scene");
+        SceneManager.LoadScene(1);   
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
